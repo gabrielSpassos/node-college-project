616af773e2ef146c4025d85064c357b320216efc
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-05-09 11:46:21 -0300

    refactor buscando dados e renderizando

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
 
-mongoose.connect("mongodb://localhost:27017/gym", { useMongoClient: true });
+mongoose.connect("mongodb://localhost:27017/gym");
 
 var dbMongo = mongoose.connection;
 
@@ -12,19 +14,17 @@ dbMongo.once('open', function () {
 	console.log('Aplicação conectada no MongoDB');
 });
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 var treino = mongoose.model('treino',
 	new mongoose.Schema({
         nomeTreino: { type: String },
-        exercicios: { type: Array }
-    }));
-    
-var exercicio = mongoose.model('treino',
-	new mongoose.Schema({
-        nomeExercicio: { type: String },
-        peso: { type: Number },
-        repeticoes: { type: Number },
-        series: {type: Number },
-        descanco: {type: String }
     }));
     
 app.get('/usuarios', function (req, res) {
@@ -48,7 +48,21 @@ app.get('/treinos', function (req, res) {
 });
 
 app.get('/treinos/id', function (req, res) {
-    res.send('treino {id} retornado!');
+    var teste = "Marley cotoco!";
+
+    treino.find({}, function (err, treinos) {
+		if (err) {
+			console.error(err);
+			res.status(500).send('Erro na aplicação: ' + err.message);
+		} else {
+            console.log("Amigo estou aqui!");
+            console.log(treinos);
+			res.render('treino', {
+                teste: teste,
+                treinos: treinos
+            });
+        }
+	});
 });
 
 app.post('/treinos/id', function (req, res) {
