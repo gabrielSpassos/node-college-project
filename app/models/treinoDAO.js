795d3405764ef0ed721e6d55f3ceae7d01b103d0
CrazyAMK <eduardo-amroginski@hotmail.com>
2018-05-23 15:03:08 -0300

    COLOCADO PARA APARECER O NOME DO USUARIO NO LUGAR DO CPF

--- app/models/treinoDAO.js
+++ app/models/treinoDAO.js
@@ -22,7 +22,7 @@ module.exports = function(){
 
     this.getTreinoDia = function(connectionMysql, idUsuario, callback) {
         console.log("retornando valor")
-        return connectionMysql.query('SELECT tue.id_usuario, d.nome_dia, t.nomeTreino, e.nomeExercicio, tue.peso, tue.repeticoes, tue.series, tue.descanso FROM treino_usuario_exercicio tue JOIN treino_exercicio te ON te.id = tue.id_treino_exercicio JOIN dia d ON d.id_dia = tue.id_dia JOIN treino t ON t.id = te.id_treino JOIN exercicio e ON e.id = te.id_exercicio WHERE tue.id_usuario = '.concat(idUsuario), callback);
+        return connectionMysql.query('SELECT u.nomecompleto, d.nome_dia, t.nomeTreino, e.nomeExercicio, tue.peso, tue.repeticoes, tue.series, tue.descanso FROM treino_usuario_exercicio tue JOIN treino_exercicio te ON te.id = tue.id_treino_exercicio JOIN dia d ON d.id_dia = tue.id_dia JOIN treino t ON t.id = te.id_treino JOIN exercicio e ON e.id = te.id_exercicio JOIN usuario u ON u.cpf_usuario = tue.id_usuario WHERE tue.id_usuario = '.concat(idUsuario), callback);
     }
 
     return this;
--- data/search_data.sql
+++ data/search_data.sql
@@ -1,4 +1,4 @@
-
+use gym
 /*Pesquisa que usaremos na pagina do treino (apenas visualização)*/
 select tue.id_usuario, d.nome_dia, t.nomeTreino, e.nomeExercicio, tue.peso, tue.repeticoes, tue.series, tue.descanso from treino_usuario_exercicio tue
 join treino_exercicio te 
@@ -9,6 +9,8 @@ join treino t
 on t.id = te.id_treino
 join exercicio e 
 on e.id = te.id_exercicio
+Join usuario u
+on u.cpf_usuario = tue.id_usuario
 where tue.id_usuario = "03726138021" and tue.id_dia = 2;
 
 select*From treino_usuario_exercicio
