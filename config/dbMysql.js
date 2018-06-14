e6334785f93a9bd639a0253ab04f315a1873ebd6
CrazyAMK <eduardo-amroginski@hotmail.com>
2018-06-14 01:05:38 -0300

    Banco de dados na Cloud adicionado

--- config/dbMysql.js
+++ config/dbMysql.js
@@ -3,8 +3,8 @@ var mysql = require('mysql'); //chama o banco de dados mysql
 var ConnectMySql = function(){
     console.log("Conexão com Banco bd foi estabelecida");
     return mysql.createConnection({ //faz a conexão com o banco de dados
-        host: 'localhost',
-        port:'3306',
+        host: '52.2.220.2',
+        port:'3636',
         user: 'root',
         password: '',
         database: 'gym'
