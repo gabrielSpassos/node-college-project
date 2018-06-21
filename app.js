a325794d0f4d0a9264d8b37dc7d0858e28f99223
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-06-21 13:42:15 -0300

    :recycle: refatorando port do serviço

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
-var app = require("./config/server");
+require('dotenv').load();
+const app = require("./config/server");
 
-//var treinoModel = require("./app/models/treinoModel");
+const PORT = process.env.PORT || 3000;
 
-
-/*var bodyParser = require('body-parser');
-
-app.get('/usuarios', function (req, res) {
-    res.send('Lista de Usuários!!!');
-});
-
-app.get('/usuarios/id', function (req, res) {
-    res.send('Usuário {id} retornado!');
-});
-
-app.post('/usuarios/id', function (req, res) {
-    res.send('Usuário {id} Cadastrado!');
-});
-
-app.put('/usuarios/id', function (req, res) {
-    res.send('Usuário {id} Alterado!');
-});
-
-app.get('/treinos', function (req, res) {
-    res.send('Lista de Treinos!');
-});
-
-app.get('/treinos/id', function (req, res) {
-    res.send('treino {id} retornado!');
-});
-
-app.post('/treinos/id', function (req, res) {
-    res.send('treino {id} Cadastrado!');
-});
-
-app.put('/treinos/id', function (req, res) {
-    res.send('treino {id} alterado!');
-});
-
-app.put('/exercicios/id', function (req, res) {
-    res.send('exercicio {id} alterado!');
-});
-*/
-app.listen(4200, function () {
-    console.log('Iniciando aplicação na porta 4200!');
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
